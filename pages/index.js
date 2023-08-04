b5b88e4c54d8e07f8f8d8e65146f7b4ad45ebe03
Luan Prates <pratesr21@gmail.com>
2023-08-04 12:06:25 +0000

    Melhorando a página de `em construção`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-  return <h1>HipnoNews</h1>;
+  return (
+    <div id="container">
+      <h1>HipnoNews</h1>
+      <p>
+        Estamos construindo um novo espaço na internet para quem trabalha e
+        estuda sobre Hipnose, precisa consumir conteúdo de qualidade e quer interagir com outras pessoas que estão na mesma jornada.
+      </p>
+    </div>
+  );
 }
 
 export default Home;
