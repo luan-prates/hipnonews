34450dbde10485974ac2758a92cc658ab67e92ca
Luan Prates <pratesr21@gmail.com>
2024-02-27 13:58:03 -0300

    finaliza o endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,23 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status")
   expect(response.status).toBe(200);
+
+  const responseBoby = await response.json();
+
+  expect(responseBoby.updated_at).toBeDefined();
+  expect(responseBoby.dependencies.database.version).toBeDefined();
+  expect(responseBoby.dependencies.database.max_connections).toBeDefined();
+  expect(responseBoby.dependencies.database.count_connections).toBeDefined();
+
+  const parsedUpdetedAt = new Date(responseBoby.updated_at).toISOString();
+  expect(responseBoby.updated_at).toEqual(parsedUpdetedAt)
+  
+  expect(responseBoby.dependencies.database.version).toEqual("16.0")
+
+  expect(responseBoby.dependencies.database.max_connections).toEqual(100)
+
+  expect(responseBoby.dependencies.database.count_connections).toEqual(1)
+
+  console.log(responseBoby)
+
 });
